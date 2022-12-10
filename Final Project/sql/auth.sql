2f729dfe60bdf8d06dcac488b4c657d7aa6308ae
DaxilPatel15 <114842131+DaxilPatel15@users.noreply.github.com>
2022-12-10 02:07:05 -0500

    Update auth.sql

--- Final Project/sql/auth.sql
+++ Final Project/sql/auth.sql
@@ -1,17 +1,23 @@
+use Daxil200520270;
 CREATE table admins(
-	ID int not null primary key auto_increment,
-	FirstName varchar (255),
-	LastName varchar (255),
-	Email varchar (255),
-	Telephone varchar(255),
-	Passwrd varchar (255)
+                       ID int primary key auto_increment,
+                       FirstName varchar (255)  ,
+                       LastName varchar (255) ,
+                       Email varchar (255) ,
+                       Telephone varchar(255)  ,
+                       Passwrd varchar (255)
 
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO admins (FirstName, LastName, Email, Telephone, Passwrd)
-VALUES
-('Bob', 'Smith', 'email1@email.ca', '705-123-4567','12345'),
-('Jane', 'Smith', 'email11@email.ca', '705-123-4567','45677'),
-('Rob', 'Doe', 'email111@email.ca', '705-123-4567','54544'),
-('Ian', 'Smith', 'email1111@email.ca', '705-123-4567','35545');
 
+
+
+
+
+
+
+
+
+select * from admins;
+
+--  drop table admins;
