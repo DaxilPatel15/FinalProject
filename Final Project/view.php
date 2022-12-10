6cdd218b8183c802661b7115702bcd539c9d3344
DaxilPatel15 <114842131+DaxilPatel15@users.noreply.github.com>
2022-12-10 02:13:18 -0500

    Update view.php

--- Final Project/view.php
+++ Final Project/view.php
@@ -1,35 +1,72 @@
 <?php
-// Include database file
-include 'database.php';
-$customerObj = new database();
-
-
-// Delete record from table
-if (isset($_GET['deleteId']) && !empty($_GET['deleteId'])) {
-    $deleteId = $_GET['deleteId'];
-    $customerObj->deleteRecord($deleteId);
+// adding database file to make connection b/t 3 files
+session_start();
+if(!isset($_SESSION['ID'])){
+    header('Location:signin.php');
+    exit();
 }
+else {
+    require "database.php";
+    $customerObj = new database();
+
+
+// adding delete to delete the record that were inserted
+    if (isset($_GET['deletedID']) && !empty($_GET['deletedID'])) {
+        $deletedID = $_GET['deletedID'];
+        $customerObj->deleteRecord($deletedID);
+
+    }
+
 ?>
 
 
-<?php
-include ('header.php')
-?>
+
+
+
+
+
+
 
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Records</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Final Project</title>
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
 </head>
 <body>
-
+  <?php
+  require('header.php');
+  ?>
 <div class="container mt-3">
     <h2>Registration Details</h2>
+    <?php
+    // adding msg statement to give infromation about the update,inserted and deleted data.
+
+    if (isset($_GET['msg2']) == "update") {
+        echo "<div class='alert alert-success alert-dismissible'>
+              <button type='button' class='close' data-dismiss='alert'>×</button>
+              Your Registration updated successfully
+            </div>";
+    }
+    if (isset($_GET['msg3']) == "delete") {
+        echo "<div class='alert alert-success alert-dismissible'>
+              <button type='button' class='close' data-dismiss='alert'>×</button>
+              Record deleted successfully
+            </div>";
+    }
+    ?>
 
     <table class="table table-dark table-hover">
         <thead>
@@ -54,11 +91,11 @@ include ('header.php')
         ?>
         <tr>
 
-            <td><?php echo $customer['FirstName'] ?></td>
-            <td><?php echo $customer['LastName'] ?></td>
-            <td><?php echo $customer['Email'] ?></td>
-            <td><?php echo $customer['Telephone'] ?></td>
-            <td><?php echo $customer['Passwrd'] ?></td>
+            <td><?php echo $customer['FirstName']; ?></td>
+            <td><?php echo $customer['LastName']; ?></td>
+            <td><?php echo $customer['Email']; ?></td>
+            <td><?php echo $customer['Telephone']; ?></td>
+            <td><?php echo $customer['Passwrd']; ?></td>
 
             <td> <div class="col-md-2">
                     <a href="update.php?updateID=<?php echo $customer['ID'];?>">
@@ -70,10 +107,17 @@ include ('header.php')
                         <input type="submit" class="profile-edit-btn" name="btnAddMore" value="Remove"></a>
                 </div></td>
         </tr>
-        <?php } ?>
+        <?php
+      }
+?>
         </tbody>
     </table>
+    <button type="button" class="btn btn-primary"><a href="logout.php" style="color: #41250b;
+    text-decoration: none;">Log Out</a></button>
 </div>
-
+<?php require'footer.php'?>
 </body>
 </html>
+<?php
+        }
+?>
