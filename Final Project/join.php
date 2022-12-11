591cdf040c61c13823738fa3dfc155b93dee99e1
DaxilPatel15 <114842131+DaxilPatel15@users.noreply.github.com>
2022-12-11 01:26:40 -0500

    Update join.php

--- Final Project/join.php
+++ Final Project/join.php
@@ -1,5 +1,10 @@
+<!--Name:Daxil Patel-->
+<!--Student ID:200520270-->
+
 <?php
+//including database file
 include "database.php";
+//using below method to send the data to database
 $customerObj = new database();
 if (isset($_POST['submit'])) {
 $customerObj->insertData($_POST);
@@ -15,17 +20,25 @@ $customerObj->insertData($_POST);
         <meta name="author" content="" />
         <title>Final Project</title>
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
+        <!-- Font Awesome icons -->
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
         <!-- Google fonts-->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet" />
-        <!-- Core theme CSS (includes Bootstrap)-->
+<!--        including css file-->
         <link href="css/styles.css" rel="stylesheet" />
     </head>
     <body>
+<!--    including global header-->
     <?php require ('header.php'); ?>
     <section class="page-section cta">
+        <?php  if (isset($_GET['msg5']) == "sameemail") {
+        echo "<div class='alert alert-success alert-dismissible'>
+            <button type='button' class='close' data-dismiss='alert'>X</button>
+            This Email is currently in use
+        </div>";
+        }
+        ?>
 
     <div class="cta-inner bg-faded text-center rounded">
         <h2 class="section-heading mb-5">
@@ -53,7 +66,7 @@ $customerObj->insertData($_POST);
             <label for="Email">Email</label>
           </div>
           <div class="form-floating">
-              <!-- adding input for Password -->
+              <!-- adding input for telephone -->
               <input type="tel" class="form-control" id="Telephone" name="Telephone"  placeholder="Telephone" required="">
               <label for="Telephone">Telephone Number</label>
           </div>
@@ -63,7 +76,7 @@ $customerObj->insertData($_POST);
             <label for="Passwrd">Password</label>
           </div>
           <div class="form-floating">
-            <!-- adding input for Password -->
+            <!-- adding input for confirm Password -->
             <input type="password" class="form-control" id="Confirmp" name="Confirmp"  placeholder="Confirm-Password" required="">
             <label for="Confirmp">Confirm Password</label>
           </div>
@@ -75,6 +88,7 @@ $customerObj->insertData($_POST);
                 <strong>If you already have an account please click the below link</strong>
                 <br />
                 <a class="nav-link text-uppercase" href="signin.php">Login</a>
+<!--                giving link for the signin page-->
             </em>
         </p>
         </form>
@@ -86,7 +100,8 @@ $customerObj->insertData($_POST);
         <?php require ('./footer.php'); ?>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
+
         <script src="js/scripts.js"></script>
     </body>
 </html>
+<!--end of file-->
